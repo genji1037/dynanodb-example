b2d80618dc15eaeb11fea9125c7355cba97aba53
yinyan.chen <386204174@qq.com>
2020-10-22 17:13:46 +0800

    fix bug

--- progress/progress.go
+++ progress/progress.go
@@ -31,11 +31,15 @@ func ObserveRead(duration time.Duration) {
 func Report() {
 	fmt.Println("=====report=====")
 
-	fmt.Println("write tps", float64(P.WriteCount)/(float64(P.TimeTotal)/float64(time.Second)))
-	fmt.Println("average write latency", time.Duration(P.WriteTimeTotal/P.WriteCount))
+	if P.WriteCount > 0 {
+		fmt.Println("write tps", float64(P.WriteCount)/(float64(P.TimeTotal)/float64(time.Second)))
+		fmt.Println("average write latency", time.Duration(P.WriteTimeTotal/P.WriteCount))
+	}
 
-	fmt.Println("read tps", float64(P.ReadCount)/(float64(P.TimeTotal)/float64(time.Second)))
-	fmt.Println("average read latency", time.Duration(P.ReadTimeTotal/P.ReadCount))
+	if P.ReadCount > 0 {
+		fmt.Println("read tps", float64(P.ReadCount)/(float64(P.TimeTotal)/float64(time.Second)))
+		fmt.Println("average read latency", time.Duration(P.ReadTimeTotal/P.ReadCount))
+	}
 
 	fmt.Println("total cost", P.TimeTotal)
 
